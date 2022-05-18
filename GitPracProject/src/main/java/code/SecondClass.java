ee252446053711b3d3cad245ba40f48c45081684
Ashutosh kumar <ashutosh.1.k@coforge.com>
2022-05-18 10:48:00 +0530

    added second class

---
+++ GitPracProject/src/main/java/code/SecondClass.java
@@ -0,0 +1,11 @@
+package code;
+
+public class SecondClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is the second class");
+
+	}
+
+}
